d0bd587d4671e8050534efc841ac708a3c3d9163
ZHU YIFAN <simba23333@gmail.com>
2019-07-19 21:36:05 +0800

    Create Euclid's algorithm.c
    Euclid's algorithm is for computing the greatest common divisor.

---
+++ All codes/Chapter  2  Algorithm Analysis/Euclid's algorithm.c
@@ -0,0 +1,12 @@
+unsigned int Gcd(unsigned int M, unsigned int N)
+{
+    unsigned int Rem;
+    
+    while (N > 0)
+    {
+        Rem = M % N;
+        M = N;
+        N = Rem;
+    }
+    return M;
+}
