d6dda5438fa5fa5af6c75a44fec2b07230b61f87
ZHU YIFAN <simba23333@gmail.com>
2019-07-23 11:42:11 +0800

    Create stack ADT.h
    Type declaration for linked list implementation of the stack ADT

---
+++ All codes/Chapter  3  Lists, Stacks, and Queues/stack ADT.h
@@ -0,0 +1,24 @@
+#ifndef _Stack_h
+
+struct Node;
+typedef struct Node *PtrToNode;
+typedef PtrToNode Stack;
+
+int IsEmpty(Stack S);
+Stack CreateStack(void);
+void DisposeStack(Stack S);
+void MakeEmpty(Stack S);
+void Push(ElementType X, Stack S);
+ElementType Top(Stack S);
+void Pop(Stack S);
+
+#endif  /* _Stack_h */
+
+
+/* Place in implementation file */
+/* Stack implementation is a linked list with a header */
+struct Node
+{
+    ElementType Element;
+    PtrToNode   Next;
+};
