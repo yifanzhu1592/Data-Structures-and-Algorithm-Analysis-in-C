ad3cff134c481af0952e5493609ed1248a6b0b78
ZHU YIFAN <simba23333@gmail.com>
2019-07-25 18:14:36 +0800

    Create binary search tree.h
    Binary search tree declarations

---
+++ All codes/Chapter  4  Trees/binary search tree.h
@@ -0,0 +1,24 @@
+#ifndef _Tree_H
+
+struct TreeNode;
+typedef struct TreeNode *Position;
+typedef struct TreeNode *SearchTree;
+
+SearchTree MakeEmpty(SearchTree T);
+Position Find(ElementType X, SearchTree T);
+Position FindMin(SearchTree T);
+Position FindMax(SearchTree T);
+SearchTree Insert(ElementType X, SearchTree T);
+SearchTree Delete(ElementType X, SearchTree T);
+ElementType Retrieve(Position P);
+
+#endif /* _Tree_H */
+
+
+/* Place in the implementation file */
+struct TreeNode
+{
+    ElementType Element;
+    SearchTree  Left;
+    SearchTree  Right;
+};
