09d1e253c1833c1e062adfdd911171ed44a48749
ZHU YIFAN <simba23333@gmail.com>
2019-07-29 16:04:17 +0800

    Create priority queue.h
    Declaration for priority queue

---
+++ All codes/Chapter  6  Priority Queues (Heaps)/priority queue.h
@@ -0,0 +1,24 @@
+#ifndef _BinHeap_H
+
+struct HeapStruct;
+typedef struct HeapStruct *PriorityQueue;
+
+PriorityQueue Initialize(int MaxElements);
+void Destroy(PriorityQueue H);
+void MakeEmpty(PriorityQueue H);
+void Insert(ElementType X, PriorityQueue H);
+ElementType DeleteMin(PriorityQueue H);
+ElementType FindMin(PriorityQueue H);
+int IsEmpty(PriorityQueue H);
+int IsFull(PriorityQueue H);
+
+#endif
+
+
+/* Place in implementation file */
+struct HeapStruct
+{
+    int Capacity;
+    int Size;
+    ElementType *Elements;
+};
